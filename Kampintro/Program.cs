60ce964b1fb2db281027a835653601cd0dc7b5ed
lenova <lenova@DESKTOP-JVV39AP>
2021-02-18 18:04:56 +0300

    Ders Sonuu

---
+++ Kampintro/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama
+            //değer tutucu, alias
+
+
+            string kategoriEtiketi = "Kategori";
+            int oğrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if(dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+           
+
+            
+
+            if (sistemeGirisYapmisMi == true) 
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+            
+
+
+            
+
+          
+
+           
+           
+
+
+        }
+    }
+}
